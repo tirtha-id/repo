9250e386d54580af37489e48d8d461f58b6babe5
tirtha-id <tirtha@rocketmail.com>
2025-04-02 10:49:41 +0700

    commit index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("Hello World...");
-console.log("Hai hai...");
+
+let user = {
+    name : "John",
+    age : 30,
+}
+
+for (let prop in user) {
+    console.log(prop + ' : ' + user[prop]);
+}
